a121eec46f4909be4b37d15a04e2ba73bd5b3c75
Erivan <erivan.nas@gmail.com>
2020-05-14 19:50:56 -0300

    Adiciona layout básico de modal children

--- src/app/custom-form-utils/components/modal-children/modal-children.component.ts
+++ src/app/custom-form-utils/components/modal-children/modal-children.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, Output, EventEmitter } from '@angular/core';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
+import { Modal } from 'src/app/ar-modal/components';
 
 @Component({
-  selector: 'app-modal-children',
+  selector: 'modal-children',
   templateUrl: './modal-children.component.html',
   styleUrls: ['./modal-children.component.css']
 })
 export class ModalChildrenComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+  @ViewChild('modalNewField') modalNewField:Modal
+  @Output('confirm') confirm: EventEmitter<any> = new EventEmitter();
+
+  constructor(private formBuilder: FormBuilder,
+    ) { 
+      this.form = this.formBuilder.group({
+        name: [null, Validators.required],
+        type: [null, Validators.required],
+      });
+    }
 
   ngOnInit(): void {
   }
+  
+  open(): void{
+    this.modalNewField.open();
+  }
 
+  onConfirm(){
+    if(this.form.valid) {
+      this.confirm.emit(this.form.value);
+      this.modalNewField.close()
+    }
+      
+  }
 }

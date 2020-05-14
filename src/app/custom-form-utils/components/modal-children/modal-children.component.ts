c0a59f6835492c5a97a624b08c0f2f338df8ff97
Erivan <erivan.nas@gmail.com>
2020-05-14 20:13:56 -0300

    carrega formulário ao clicar duas vezes

--- src/app/custom-form-utils/components/modal-children/modal-children.component.ts
+++ src/app/custom-form-utils/components/modal-children/modal-children.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, ViewChild, Output, EventEmitter, Input } from '@angular/core';
 import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 import { Modal } from 'src/app/ar-modal/components';
 
@@ -12,16 +12,45 @@ export class ModalChildrenComponent implements OnInit {
   form: FormGroup;
   @ViewChild('modalNewField') modalNewField:Modal
   @Output('confirm') confirm: EventEmitter<any> = new EventEmitter();
+  @Input('child') child;
 
   constructor(private formBuilder: FormBuilder,
     ) { 
       this.form = this.formBuilder.group({
-        name: [null, Validators.required],
-        type: [null, Validators.required],
+        group: null,
+        orderResume: null,
+        groupResume: null,
+        fieldName: null,
+        label: null,
+        type: null,
+        display: null,
+        required: null,
+        mask: null,
+        editable: null,
+        size: null,
+        capitalization: null,
       });
     }
 
   ngOnInit(): void {
+  }
+
+  ngOnChanges() {
+    const child = this.child;
+    this.form.patchValue({
+      group: child.group,
+      orderResume: child.orderResume,
+      groupResume: child.groupResume,
+      fieldName: child.fieldName,
+      label: child.label,
+      type: child.type,
+      display: child.display,
+      required: child.required,
+      mask: child.mask,
+      editable: child.editable,
+      size: child.size,
+      capitalization: child.capitalization,
+    })
   }
   
   open(): void{
--- src/app/domain/TextField.ts
+++ src/app/domain/TextField.ts
@@ -3,7 +3,6 @@ import { FieldBase } from './field-base';
 export class TextField extends FieldBase {
     mask: string;
     editable: boolean;
-    display: boolean;
     size: number;
     capitalization: boolean;
 
@@ -11,7 +10,6 @@ export class TextField extends FieldBase {
         super();
         this.mask = null;
         this.editable = null;
-        this.display = null;
         this.size = null;
         this.capitalization = null;
     }
